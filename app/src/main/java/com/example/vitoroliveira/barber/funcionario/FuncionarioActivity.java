14eb693ebf9d855855cc72143dd3f1b47764d5b3
Vitor Oliveira <vitoroliveira.ads@gmail.com>
2018-05-08 19:30:03 -0300

    Importar Materialize e criação de algumas activities

--- app/src/main/java/com/example/vitoroliveira/barber/funcionario/FuncionarioActivity.java
+++ app/src/main/java/com/example/vitoroliveira/barber/funcionario/FuncionarioActivity.java
@@ -1,16 +1,13 @@
-package com.example.vitoroliveira.barber;
+package com.example.vitoroliveira.barber.funcionario;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final int MAIN_REQUEST_CODE = 1;
-
+public class FuncionarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_funcionario);
     }
 }
