a3872b6e83cc6a232df362129f78800c9518a2cd
Luhan Lacerda <luhanlacerda@gmail.com>
2018-05-12 12:25:09 -0300

    Criacao da activity listar cliente, cadastrar e listar servico, e inicio da cadastrar adm

---
+++ app/src/main/java/com/example/vitoroliveira/barber/servico/ListServicoActivity.java
@@ -0,0 +1,15 @@
+package com.example.vitoroliveira.barber.servico;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.vitoroliveira.barber.R;
+
+public class ListServicoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_servico);
+    }
+}
