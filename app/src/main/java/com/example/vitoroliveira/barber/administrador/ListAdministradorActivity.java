f21e02f37d6582392788416c5a8c8b73e0073250
Luhan Lacerda <luhanlacerda@gmail.com>
2018-05-12 12:37:47 -0300

    Criação da activity Cadastro e Listagem Administrador e inclusão de ID nos edittexts e buttons

---
+++ app/src/main/java/com/example/vitoroliveira/barber/administrador/ListAdministradorActivity.java
@@ -0,0 +1,13 @@
+package com.example.vitoroliveira.barber.administrador;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListAdministradorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_administrador);
+    }
+}
