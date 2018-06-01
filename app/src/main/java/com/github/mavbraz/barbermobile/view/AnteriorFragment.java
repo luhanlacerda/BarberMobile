a176adae50517ef2c670f3ec89a18f262c6554b4
Vitor Oliveira <vitoroliveira.ads@gmail.com>
2018-05-31 21:27:28 -0300

    Criacao da activity Agenda.

---
+++ app/src/main/java/com/github/mavbraz/barbermobile/view/AnteriorFragment.java
@@ -0,0 +1,83 @@
+package com.github.mavbraz.barbermobile.view;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mavbraz.barbermobile.R;
+
+
+public class AnteriorFragment extends Fragment {
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private OnFragmentInteractionListener mListener;
+
+    public AnteriorFragment() {
+        // Required empty public constructor
+    }
+
+
+    public static AnteriorFragment newInstance(String param1, String param2) {
+        AnteriorFragment fragment = new AnteriorFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_anterior, container, false);
+    }
+
+    // TODO: Rename method, update argument and hook method into UI event
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
---
+++ app/src/main/java/com/github/mavbraz/barbermobile/view/SelecionarServicoActivity.java
@@ -0,0 +1,66 @@
+package com.github.mavbraz.barbermobile.view;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.github.mavbraz.barbermobile.R;
+import com.github.mavbraz.barbermobile.model.basicas.Servico;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SelecionarServicoActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selecionar_servico);
+
+        List<Servico> servicos = Arrays.asList(
+                new Servico(1, "Corte de cabelo", "Fique como o Chewbacca", 30.5),
+                new Servico(2, "Barba e bigode", "Fique como a barbie", 150.8));
+
+        listView = findViewById(R.id.list_servicos);
+        listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        listView.setAdapter(new ListarServicosAdapter(this, servicos));
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                if (listView.isItemChecked(position)) {
+                    view.setBackgroundColor(Color.GRAY);
+                    listView.setItemChecked(position, true);
+                } else {
+                    view.setBackgroundColor(Color.WHITE);
+                    listView.setItemChecked(position, false);
+                }
+
+            }
+        });
+
+        findViewById(R.id.confirmar_servico).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.confirmar_servico) {
+            List<Integer> checked = new ArrayList<>();
+            for (int i = 0; i < listView.getCheckedItemPositions().size(); i++) {
+                if (listView.getCheckedItemPositions().valueAt(i)) {
+                    checked.add(listView.getCheckedItemPositions().keyAt(i));
+                }
+            }
+
+            Toast.makeText(this, checked + "", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
+
