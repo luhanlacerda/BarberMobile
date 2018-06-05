1ab4eab61afb7eccc788d2a88274f9d9b321a7ef
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-06-05 01:02:15 -0300

    todo task

--- app/src/main/java/com/github/mavbraz/barbermobile/services/ServiceBuilder.java
+++ app/src/main/java/com/github/mavbraz/barbermobile/services/ServiceBuilder.java
@@ -1,5 +1,20 @@
 package com.github.mavbraz.barbermobile.services;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class ServiceBuilder {
+
+  private static final String URL = "http://localhost:3000";
+
+  // TODO: CREATE LOGGER
+  private static Retrofit.Builder builder = new Retrofit.Builder()
+      .baseUrl(URL).addConverterFactory(GsonConverterFactory.create());
+
+  public static Retrofit retrofit = builder.build();
+
+  public static <S> S buildService(Class<S>  serviceType){
+    return retrofit.create(serviceType);
+  }
 
 }
