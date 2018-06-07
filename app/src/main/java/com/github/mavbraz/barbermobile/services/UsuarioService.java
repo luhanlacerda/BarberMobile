cf261be7a34ffc5412531f786f2be0ae1fbf348a
Rodolfo Estevam <rodolfoestevam.a@gmail.com>
2018-06-07 01:02:00 -0300

    alteracao no metodo do service

--- app/src/main/java/com/github/mavbraz/barbermobile/services/UsuarioService.java
+++ app/src/main/java/com/github/mavbraz/barbermobile/services/UsuarioService.java
@@ -5,12 +5,13 @@ import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface UsuarioService {
 
   @POST("usuario/registrar/cliente")
   Call<Cliente> registrarUsuario(@Body Cliente cliente);
 
-  @POST("usuario/login/cliente")
-  Call<Cliente> loginUsuario(@Body Cliente cliente);
+  @GET("usuario/login/cliente/{email}/{password}")
+  Call<Cliente> loginUsuario(@Path("email") String  email, @Path("password") String password);
 }
